ea8226b468b38f727a321ca1cac2f587e485ae64
HelarJ <HelarJ@users.noreply.github.com>
2020-12-09 07:26:40 +0200

    Task #2, #3 implemented PUT and DELETE endpoints for liking/unliking posts.

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -37,11 +37,21 @@ router.post('/', authorize,  (request, response) => {
 router.put('/:postId/likes', authorize, (request, response) => {
 
     // Endpoint for current user to like a post
+
+    PostModel.like(request.currentUser.id, request.params.postId, (callback) => {
+        response.status(200).json([])
+    });
+
+
 });
 
 router.delete('/:postId/likes', authorize, (request, response) => {
 
     // Endpoint for current user to unlike a post
+
+    PostModel.unlike(request.currentUser.id, request.params.postId, (callback) => {
+        response.status(200).json([])
+    });
 
 });
 
